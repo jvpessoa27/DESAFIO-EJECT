fb6e05cd0446f9b04287683a6535ca0010031a81
João Victor <jvpessoa27@hotmail.com>
2022-10-20 15:10:02 -0300

    Deploy

--- site_institucional/settings.py
+++ site_institucional/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i=0(*rg+33a^l@iwd&t)33!w^=oj$a%o@l&!_trc=mzniw+q^3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.pythonanywhere.com']
 
 
 # Application definition
